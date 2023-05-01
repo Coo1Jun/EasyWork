0da2e7069e181c4fcd46bfb3816f884c3fc8ac13
CoolJun <1425176577@qq.com>
2023-05-01 15:36:04 +0800

    fix:【ew-project】添加工作项的邮件预警

--- ew-project/src/main/java/com/ew/project/schedule/WorkItemTask.java
+++ ew-project/src/main/java/com/ew/project/schedule/WorkItemTask.java
@@ -1,18 +1,25 @@
 package com.ew.project.schedule;
 
 import cn.edu.hzu.client.dto.NotificationAddParam;
+import cn.edu.hzu.client.dto.UserDto;
 import cn.edu.hzu.client.server.service.ICommunicationClientService;
+import cn.edu.hzu.client.server.service.IServerClientService;
 import cn.edu.hzu.common.api.utils.DateUtils;
 import cn.edu.hzu.common.api.utils.StringUtils;
 import cn.edu.hzu.common.constant.NotificationType;
+import cn.edu.hzu.common.service.MailService;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.ew.project.project.dto.ProjectDto;
+import com.ew.project.project.service.IProjectService;
 import com.ew.project.workitem.dto.WorkItemDto;
 import com.ew.project.workitem.service.IWorkItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
 import java.util.Date;
 import java.util.List;
 
@@ -29,10 +36,17 @@ public class WorkItemTask {
     private IWorkItemService workItemService;
     @Autowired
     private ICommunicationClientService communicationClientService;
+    @Autowired
+    private IProjectService projectService;
+    @Autowired
+    private IServerClientService serverClientService;
+    @Autowired
+    private MailService mailService;
 
 //    @Scheduled( cron = "0 0 8 * * ?") // 每天上午8点
 //    @Scheduled( cron = "0 48 19 * * ?") // 每天19点48分
     @Scheduled( cron = "0 0 12 * * ?") // 每天中午12点
+//    @Scheduled(fixedDelay = 5000) //上一次执行完毕时间点之后5秒再执行
     public void scheduledMethod() {
         log.info("=============>>>>>>> 定时任务 start <<<<<<<<<<<<<<=======================");
         Date now = new Date();
@@ -49,6 +63,24 @@ public class WorkItemTask {
                     notificationAddParam.setUserId(workItem.getPrincipalId());
                     notificationAddParam.setOperationId(workItem.getId());
                     communicationClientService.addNotification(notificationAddParam);
+                    // 获取项目信息
+                    ProjectDto projectDto = projectService.getDtoById(workItem.getProjectId());
+                    // 获取用户信息，从而获取用户邮箱
+                    UserDto userDto = serverClientService.getUserDtoById(workItem.getPrincipalId());
+                    // 发送邮件
+                    String subject = "Easy Work 预警";
+                    String templateName = "work-item-warning";
+                    Context context = new Context();
+                    context.setVariable("projectTab", projectDto.getProjectTab());
+                    context.setVariable("number", workItem.getNumber());
+                    context.setVariable("workItemTitle", workItem.getTitle());
+                    context.setVariable("endTime", DateUtils.parseDateToStr("yyyy-MM-dd HH:mm:ss", workItem.getEndTime()));
+                    try {
+                        mailService.sendMail(userDto.getEmail(), subject, templateName, context);
+                        log.info(">>>>>>>>>>>邮件预警发送成功，用户邮箱===》【{}】<<<<<<<<<<<<<<<<", userDto.getEmail());
+                    } catch (MessagingException e) {
+                        log.warn(">>>>>>>>>>>>>>>>邮件预警发送失败<<<<<<<<<<<<<<<<<");
+                    }
                 }
             }
         }
